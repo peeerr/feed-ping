bbf25511885c3d11d2fb14b7db8815b2936463b0
JunHyeongChoi <ajfeh22@gmail.com>
2025-02-24 01:26:03 +0900

    feat: CORS 설정

---
+++ backend/src/main/java/com/feedping/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.feedping.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://feedping.co.kr", "http://feedping.co.kr")
+                .allowedMethods("GET", "POST", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+
+}
